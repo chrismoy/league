a901998e1e888990dd5b544f1a448754bcab821d
Chris Moy <chris@wetbar.co>
2015-01-28 15:59:11 -0600

    Work on layout and routes

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  var viewportHeight = $(window).height();
+  var gameAlertHeight = $('#game-div').height();
+  var topbarHeight = $('#mobile-top-bar').height()
+                      || $('#desktop-top-bar').height();
+  var mapbarHeight = $('#mapbar').height();
+  var signupHeight = $('#signup').height()
+
+  var mainHeight = viewportHeight -  gameAlertHeight - topbarHeight
+                      - mapbarHeight - signupHeight;
+
+  $(window).resize(function(){
+    console.log( mainHeight )
+    $("#main-content").css( "height", mainHeight);
+  });
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root  'main_pages#home'
-  get   'main_pages/help'
-  get   'main_pages/about'
 
+  root               'main_pages#home'
+  get 'help'      => 'main_pages#help'
+  get 'about'     => 'main_pages#about'
+  get 'contact'   => 'main_pages#contact'
+  get 'signup'    => 'users#new'
 end
