83ee9be18c024f2308d3c655a5767717aa209f3b
Chris Moy <chris@wetbar.co>
2015-02-01 15:40:10 -0600

    Add log in and log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
-  root               'main_pages#home'
-  get 'help'      => 'main_pages#help'
-  get 'about'     => 'main_pages#about'
-  get 'contact'   => 'main_pages#contact'
-  get 'signup'    => 'users#new'
+  root                   'main_pages#home'
+  get     'help'      => 'main_pages#help'
+  get     'about'     => 'main_pages#about'
+  get     'contact'   => 'main_pages#contact'
+  get     'signup'    => 'users#new'
+  get     'login'     => 'sessions#new'
+  post    'login'     => 'sessions#create'
+  delete  'logout'    => 'sessions#destroy'
   resources :users
 end
