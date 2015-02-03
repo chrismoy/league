e80256e7e67c635d5a9014e1edd99a9966b81e82
Chris Moy <chris@wetbar.co>
2015-02-03 17:48:56 -0600

    Finish user CRUD actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if current user is given
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the current logged-in user (if present)
   def current_user
     if (user_id = session[:user_id])
@@ -42,5 +47,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to either the stored url or the default
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the url for use after login
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
