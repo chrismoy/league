f1995bfdc3f1cf64b0a0ac15816a96d5c5b871ff
Chris Moy <chris@wetbar.co>
2015-02-05 14:51:30 -0600

    Refactor activation and password reset actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,7 @@ class User < ActiveRecord::Base
 
   # Activate an account
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Send activation email
@@ -55,8 +54,8 @@ class User < ActiveRecord::Base
   # Sets password reset attributes
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,   User.digest(reset_token))
-    update_attribute(:reset_sent_at,  Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                    reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -31,6 +31,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # Try to log in before activation.
     log_in_as(user)
     assert_not is_logged_in?
+    # Index page
+    get users_path
+    assert_no_match user.name, response.body
+    # Profile page
+    get user_path(user)
+    assert_redirected_to root_url
     # Invalid activation token
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
