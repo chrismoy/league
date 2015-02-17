a07f5f9af19f36751fb2beb364344c1a6f2404a5
Chris Moy <chris@wetbar.co>
2015-02-17 16:41:30 -0600

    Fix main content js resize event

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,7 @@
-$(document).ready(function() {
-
+var resizeViewPort = function() {
   var viewportHeight = $(window).height();
   var gameAlertHeight = $('#game-div').height();
-  var topbarHeight = $('#mobile-top-bar').height()
-                      || $('#desktop-top-bar').height();
+  var topbarHeight = $('#mobile-top-bar').height() || $('#desktop-top-bar').height();
   var mapbarHeight = $('#mapbar').height();
   var signupHeight = $('#signup').height()
   var joinGameHeight = $('#join-game').height()
@@ -11,8 +9,13 @@ $(document).ready(function() {
   var mainHeight = viewportHeight -  gameAlertHeight - topbarHeight
                       - mapbarHeight - signupHeight - joinGameHeight;
 
-  $(window).resize(function(){
-    $(".content").css( "height", mainHeight);
-  });
+  $(".content").height(mainHeight);
+};
 
+$(document).ready(function(){
+  resizeViewPort();
+
+  $(document).on("page:load", function(){
+    resizeViewPort();
+  });
 });
