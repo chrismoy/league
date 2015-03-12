3511d6b62599be92b1db0cd41f7502c3031a7792
Chris Moy <moyboy530@gmail.com>
2015-03-11 23:32:18 -0500

    Update migrations to only add float lat and lng

--- db/migrate/20150304221952_add_latitude_and_longitude_to_location.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLongitudeToLocation < ActiveRecord::Migration
-  def change
-    add_column :parks, :latitude, :string
-    add_column :parks, :longitude, :string
-  end
-end
--- db/migrate/20150311193647_remove_latitude_and_longitude_from_parks.rb
+++ db/migrate/20150311193647_remove_latitude_and_longitude_from_parks.rb
@@ -1,7 +1,5 @@
-class RemoveLatitudeAndLongitudeFromParks < ActiveRecord::Migration
+class AddLatitudeAndLongitudeToParks < ActiveRecord::Migration
   def change
-    remove_column :parks, :latitude
-    remove_column :parks, :longitude
     add_column :parks, :latitude, :float
     add_column :parks, :longitude, :float
   end
