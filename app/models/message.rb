1be0796524259655602fd7be85aae4f561062c8f
Chris Moy <moyboy530@gmail.com>
2015-04-08 15:34:22 -0500

    Make chat more forum-esque, add validations to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :game_id, presence: true
+  validates :content, presence: true
 end
