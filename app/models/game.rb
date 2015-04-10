06bcd7c139064f2d16431c9a8cb8485afc53100e
Chris Moy <moyboy530@gmail.com>
2015-04-09 22:53:15 -0500

    Make the main page more user friendly

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   belongs_to :court
   has_and_belongs_to_many :users
-  has_many :messages
+  has_many :messages, dependent: :destroy
   default_scope -> { order(time: :asc) }
   validates :court_id, presence: true
   validates :time, presence: true
