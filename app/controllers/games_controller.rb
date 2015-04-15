43f3d52ae46b69f4fd4b5da292445109c6e2e819
Chris Moy <moyboy530@gmail.com>
2015-04-15 17:18:11 -0500

    Troubleshoot timezone in controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,7 +9,7 @@ class GamesController < ApplicationController
       return
     end
 
-    @time = Time.parse(params[:game][:time], Time.now)
+    @time = Time.parse(params[:game][:time], Time.now.in_time_zone)
 
     if @time.hour < 8 || @time.hour > 20
       message += ", the court is not open during this time"
@@ -25,7 +25,7 @@ class GamesController < ApplicationController
       return
     end
 
-    if @time < (Time.now.in_time_zone("Central Time (US & Canada)") + 1.hour)
+    if @time < (Time.now.in_time_zone + 1.hour)
       message += ", registration for this time has closed"
       flash[:warning] = message
       redirect_to root_url
