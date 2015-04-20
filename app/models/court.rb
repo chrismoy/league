c405ca682cda07b0188a1f094a703ff29a50fe76
Chris Moy <moyboy530@gmail.com>
2015-04-20 15:28:35 -0500

    remove whenever and work on create games court instance method

--- app/models/court.rb
+++ app/models/court.rb
@@ -3,4 +3,13 @@ class Court < ActiveRecord::Base
   has_many :games
   validates :park_id, presence: true
   validates :img, presence: true
+
+  def generate_games gameDay, interval
+    time = gameDay.change(hour: 17, min: 0)
+    while time.hour < 19 do
+      game = Game.create(court_id: self.id, time: time)
+      self.games << game
+      time += interval
+    end
+  end
 end
