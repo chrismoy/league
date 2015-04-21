17e5c398c9906ac88bdedd87f074c445de0af76c
Chris Moy <moyboy530@gmail.com>
2015-04-20 19:43:50 -0500

    Add script tracking to button html in signup

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#signup-button').click(function() {
-    ga('send', 'event', 'button', 'click', 'signup-button', 1);
-    console.log('yoloist');
+    console.log('yolo');
   });
 });
