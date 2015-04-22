f073fef005b319d6897d3cf450e8a234cef24cc9
Chris Moy <moyboy530@gmail.com>
2015-04-22 05:20:22 -0500

    Remove logged in before action for game create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  # before_action :logged_in_user, only: [:create]
 
   def create
     message = "Invalid time"
