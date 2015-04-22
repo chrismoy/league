6a50da88a82ae7a45157eb5b474b1b65738d5da6
Chris Moy <moyboy530@gmail.com>
2015-04-22 00:18:28 -0500

    Allow facebook login as well

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
+  provider :facebook, ENV['facebook_id'], ENV['facebook_secret']
 end
