96b61f6a482e092cdbec0bc502edb379a7f66fee
Chris Moy <moyboy530@gmail.com>
2015-05-05 00:45:26 -0500

    Make twitter picture original sized

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret'],
                       {
                         :secure_image_url => 'true',
-                        :image_size => 'bigger'
+                        :image_size => 'original'
                       }
   provider :facebook, ENV['facebook_id'], ENV['facebook_secret'],
                       :secure_image_url => 'true', :image_size => 'large', :display => 'popup'
