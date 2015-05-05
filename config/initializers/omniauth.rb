598239063b39059714d78f9a286b1f68d03f19b1
Chris Moy <moyboy530@gmail.com>
2015-05-05 00:07:36 -0500

    Add secure image url and image size options to oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
-  provider :facebook, ENV['facebook_id'], ENV['facebook_secret']
+  provider :twitter, ENV['twitter_key'], ENV['twitter_secret'],
+                     secure_image_url: true, image_size: 'bigger'
+  provider :facebook, ENV['facebook_id'], ENV['facebook_secret'],
+                      secure_image_url: true, image_size: 'large'
 end
