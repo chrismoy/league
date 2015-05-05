095fd0a8ab65bb4d21471109d2ce3068f695c308
Chris Moy <moyboy530@gmail.com>
2015-05-05 00:14:55 -0500

    Try making options into hash

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret'],
-                     secure_image_url: true, image_size: 'bigger'
+                     { secure_image_url: 'true', image_size: 'bigger' }
   provider :facebook, ENV['facebook_id'], ENV['facebook_secret'],
-                      secure_image_url: true, image_size: 'large'
+                     { secure_image_url: 'true', image_size: 'large' }
 end
