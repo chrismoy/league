3e18e005abbea062f8e0b5735f7510ae4ed926a9
Chris Moy <moyboy530@gmail.com>
2015-05-19 12:31:13 -0500

    Send email notification

--- app/controllers/game_participations_controller.rb
+++ app/controllers/game_participations_controller.rb
@@ -9,10 +9,13 @@ class GameParticipationsController < ApplicationController
            current_user.games_today.count >= 4)
       @game.users << current_user
       @join_game = 1
+      UserMailer.game_notification(current_user, @game).deliver
     end
 
     respond_to do |format|
-      format.html { redirect_to :back, notice: "Joined Game" }
+      format.html { redirect_to root_url,
+                    notice: "You Have Joined The Game. You will receive and email confirmation shortly."
+      }
       format.js
     end
   end
