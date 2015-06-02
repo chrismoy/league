1df8eae71dab115e7b67e7a03f8fce1919b5def7
Chris Moy <moyboy530@gmail.com>
2015-06-01 23:44:39 -0500

    Use new grid for game select

--- app/helpers/parks_helper.rb
+++ app/helpers/parks_helper.rb
@@ -1,2 +1,15 @@
 module ParksHelper
+  $app_blue = "#5481DD"
+  $app_red = "#EB6874"
+  # Takes in the count of players and returns a color
+  def assign_game_color count
+    case count
+    when 0
+      return "transparent"
+    when 6
+      return $app_red
+    else
+      return $app_blue
+    end
+  end
 end
