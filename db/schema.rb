4f421a862c03006afdf5b2c3e2102d0be1fe18be
Chris Moy <moyboy530@gmail.com>
2015-06-19 23:18:23 -0500

    add timezone attribute to users and make main zone UTC

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617181436) do
+ActiveRecord::Schema.define(version: 20150620033629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20150617181436) do
     t.string   "token"
     t.string   "secret"
     t.integer  "organization_id"
+    t.string   "timezone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
